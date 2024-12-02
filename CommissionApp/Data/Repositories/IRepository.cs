85767cc508503af3145d21c137674cade850652a
gregpec <gregorpec@yahoo.com>
2024-12-02 19:26:30 +0100

    audit.txt

--- CommissionApp/Data/Repositories/IRepository.cs
+++ CommissionApp/Data/Repositories/IRepository.cs
@@ -3,7 +3,7 @@ namespace CommissionApp.Data.Repositories;
 public interface IRepository<T> : IReadRepository<T>, IWriteRepository<T>
     where T : class, IEntity
 {
-    public const string auditFileName = "audit.txt";
+    public const string auditFileName = "Resources\\Files\\audit.txt";
     event EventHandler<T>? ItemAdded;
     event EventHandler<T>? ItemRemoved;
 
