7dfbe9decb8003fdb23752ea170a1e4415af973b
Grzegorz Pec <gpeksa@interia.pl>
2025-01-02 21:23:52 +0100

    added method TryIfDataBaseIsConnect

--- CommissionApp/Program.cs
+++ CommissionApp/Program.cs
@@ -10,12 +10,38 @@ using CommissionApp.Data.Repositories;
 using CommissionApp.Services.RepositoriesServices;
 using CommissionApp.Services.FilesServices.JsonFile.ExportCsvToJsonFile;
 using CommissionApp.Services.FilesServices.JsonFile;
-using CommissionApp.Data.Repositories.Extensions;
 using CommissionApp.Components.DataProviders;
 
-class program
+class Program
 {
-    static void Main()
+    static void TryIfDataBaseIsConnect()
+    {
+        var options = new DbContextOptionsBuilder<CommissionAppSQLDbContext>()
+            .UseSqlServer("Data Source=LAPTOP-8QEUHJMJ\\SQLEXPRESS;Initial Catalog=\"CarsStorage\";Integrated Security=True;Trust Server Certificate=True")
+            .Options;
+
+        using var dbContext = new CommissionAppSQLDbContext(options);
+        try
+        {
+            if (dbContext.Database.CanConnect())
+            {
+                Console.WriteLine("Connection to SQL Server established successfully!!");
+                Thread.Sleep(2000);
+            }
+            else
+            {
+                Console.WriteLine("Failed to connect to the database");
+                return;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Failed to connect to SQL Server: {ex.Message}");
+            return;
+        }
+    }
+
+    static void Main(string[] args)
     {
         var services = new ServiceCollection();
 
@@ -34,42 +60,8 @@ class program
         services.AddDbContext<CommissionAppSQLDbContext>(options => options
         .UseSqlServer("Data Source=LAPTOP-8QEUHJMJ\\SQLEXPRESS;Initial Catalog=\"CarsStorage\";Integrated Security=True;Trust Server Certificate=True"));
 
-       
-        var options = new DbContextOptionsBuilder<CommissionAppSQLDbContext>()
-            .UseSqlServer("Data Source=LAPTOP-8QEUHJMJ\\SQLEXPRESS;Initial Catalog=\"CarsStorage\";Integrated Security=True;Trust Server Certificate=True")
-            .Options;
-
-        using var dbContext = new CommissionAppSQLDbContext(options);
-        try
-        {
-            if (dbContext.Database.CanConnect())
-            {
-                Console.WriteLine("Connection to SQL Server established successfully!!");
-            }
-            else
-            {
-                Console.WriteLine("Failed to connect to the database");
-                return;
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Failed to connect to SQL Server: {ex.Message}");
-            return;
-        }
-
-        var dbHelper = new DatabaseHelper("Data Source=LAPTOP-8QEUHJMJ\\SQLEXPRESS;Initial Catalog=\"CarsStorage\";Integrated Security=True;Trust Server Certificate=True");
-
-        bool isIdentity = dbHelper.IsIdentityColumn("Customers", "Id");
-
-        if (isIdentity)
-        {
-            Console.WriteLine("The 'Id' column in the 'Customers' table is an IDENTITY column.");
-        }
-        else
-        {
-            Console.WriteLine("The 'Id' column in the 'Customers' table is NOT an IDENTITY column.");
-        }
+        TryIfDataBaseIsConnect();
+     
         services.AddTransient<IAudit>(provider =>
         {
             string action = "[item Added!]";
@@ -82,7 +74,7 @@ class program
         var serviceProvider = services.BuildServiceProvider();
 
         var app = serviceProvider.GetService<IApp>()!;
-
+       
         app.Run();
     }
 }
