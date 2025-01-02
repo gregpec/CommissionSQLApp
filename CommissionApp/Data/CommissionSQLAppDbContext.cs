5ec2277b5b4b1ecefd877dc2b0200b7952b13149
Grzegorz Pec <gpeksa@interia.pl>
2025-01-02 21:33:00 +0100

    DbContextOptionsBuilder

--- CommissionApp/Data/CommissionSQLAppDbContext.cs
+++ CommissionApp/Data/CommissionSQLAppDbContext.cs
@@ -10,6 +10,4 @@ public class CommissionAppSQLDbContext : DbContext
         }
         public DbSet<Car> Cars { get; set; }
         public DbSet<Customer> Customers { get; set; }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    { }
 }
